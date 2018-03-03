f6c46c3de21a12e8fbeee1899e66c2a68ce5d5e5
Aditya Kothari <kothariaditya7@gmail.com>
2018-03-03 13:17:51 -0800

    Sorting and API standards

--- web/app/controllers/images_controller.rb
+++ web/app/controllers/images_controller.rb
@@ -1,12 +1,17 @@
 class ImagesController < ApplicationController
   skip_before_action :verify_authenticity_token
+
   def index
-    @image = Image.all
+    @image = Image.all.order("created_at DESC")
     render json: @image
   end
 
   def create
     @image = Image.new(user_name: params[:user_name], url: params[:url])
     @image.save
+    render json: {
+      message: "Successfully created image",
+      image: @image
+      }, status: 200
   end
 end
