6573a9e71217964c64df206a6614a55d3aea8721
Aditya Kothari <kothariaditya7@gmail.com>
2018-03-03 12:24:38 -0800

    controller with images

--- web/app/controllers/images_controller.rb
+++ web/app/controllers/images_controller.rb
@@ -1,4 +1,12 @@
 class ImagesController < ApplicationController
+  skip_before_action :verify_authenticity_token
   def index
+    @image = Image.all
+    render json: @image
+  end
+
+  def create
+    @image = Image.new(user_name: params[:user_name], url: params[:url])
+    @image.save
   end
 end
--- web/config/routes.rb
+++ web/config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   get 'images/index'
 
   root 'images#index'
+
+  post '/images/create', to: 'images#create'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
