22000ab2158dd97e44264d5651638b9e5216fe72
ey1031 <eyudeveloper@gmail.com>
2018-03-03 04:43:59 -0800

    autolayout in 2018 lul

--- poem-picture/HomeController.swift
+++ poem-picture/HomeController.swift
@@ -13,19 +13,19 @@ let image = nil;
 
 class HomeController: ViewController {
     @IBAction func start(_ sender: Any) {
-        if UIDevice.current.orientation == UIDeviceOrientation.portrait || UIDevice.current.orientation == UIDeviceOrientation.portraitUpsideDown {
-                let alertController = UIAlertController(title: "Error", message: "Please turn your device horizontallly.", preferredStyle: .alert)
-                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
-                alertController.addAction(defaultAction)
-                self.present(alertController, animated: true, completion: nil)
-        }
-            else {
+//        if UIDevice.current.orientation == UIDeviceOrientation.portrait || UIDevice.current.orientation == UIDeviceOrientation.portraitUpsideDown {
+//                let alertController = UIAlertController(title: "Error", message: "Please turn your device horizontallly.", preferredStyle: .alert)
+//                let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+//                alertController.addAction(defaultAction)
+//                self.present(alertController, animated: true, completion: nil)
+//        }
+//            else {
             let storyboard = UIStoryboard(name: "Main", bundle: nil);
             let ivc = storyboard.instantiateViewController(withIdentifier: "picture");
             ivc.modalPresentationStyle = .custom;
             ivc.modalTransitionStyle = .crossDissolve;
             self.present(ivc, animated: true);
-        }
+//        }
     }
     override func viewDidLoad() {
         super.viewDidLoad()
