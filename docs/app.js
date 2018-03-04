eea132d424707a999fa3c26a74cfd3cbf5504ab9
ey1031 <eyudeveloper@gmail.com>
2018-03-03 16:07:16 -0800

    leftover node stuff

--- docs/app.js
+++ docs/app.js
@@ -6,13 +6,16 @@ function httpGet(theUrl) {
 	return xmlHttp.responseText;
 }
 
+
 const JSON_input = JSON.parse(httpGet('https://cors-anywhere.herokuapp.com/https://ancient-plateau-48847.herokuapp.com/'))
 let container = document.getElementById("container")
 
 for (let elem_index in JSON_input) {
-	let base64Decoded = atob(elem.url)
 	let elem = JSON_input[elem_index]
 	let elem2 = JSON_input[elem_index + 1]
+	let base64Decoded = decodeURIComponent( escape( window.atob( elem.url ) ) )
+	console.log(elem.url)
+	console.log(base64Decoded)
 	container.innerHTML += "<div class = 'row'>\
 								<div class = 'half-row'>\
 									<p class = 'top'>" + elem.line1 + "</p>\
@@ -24,7 +27,7 @@ for (let elem_index in JSON_input) {
 									<p class = 'top'>" + elem.line1 + "</p>\
 									<p class = 'mid'>" + elem.line2 + "</p>\
 									<p class = 'bot'>" + elem.line3 + "</p>\
-									<img src = 'data:image/png;base64," + base64Decoded + "'>\
+									<img src = 'data:image/jpg;base64," + base64Decoded + "'>\
 								</div>\
 							</div>"
 }
