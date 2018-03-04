2328b6baa2e4e79f15bf27a69d92c89d158f36aa
ey1031 <eyudeveloper@gmail.com>
2018-03-03 20:00:32 -0800

    fix dumb web app

--- docs/app.js
+++ docs/app.js
@@ -1,3 +1,4 @@
+
 function httpGet(theUrl) {
 	console.log(theUrl)
 	var xmlHttp = new XMLHttpRequest();
@@ -8,37 +9,46 @@ function httpGet(theUrl) {
 
 
 const JSON_input = JSON.parse(httpGet('https://cors-anywhere.herokuapp.com/https://ancient-plateau-48847.herokuapp.com/'))
-let container = document.getElementById("container")
+// const JSON_input = JSON.parse(temp)
+var container = document.getElementById("container")
 
+// console.log(container)
+console.log(JSON_input)
+
+let toAdd = ""
+let count = 0
 for (let elem_index in JSON_input) {
-	console.log(elem_index)
+	// console.log("INDEX" + elem_index)
 	let elem = JSON_input[elem_index]
-	console.log(elem)
-	// let elem2 = JSON_input[elem_index + 1]
-	// var url = elem.url.replace(" ", "+").trim()
+	// console.log(elem)
 	var url = elem.url // first
-	// var url2 = elem.url2 // second
-	console.log(url)
-	// console.log(elem.url.charAt(first_break))
-	// console.log(base64Decoded)
-	// first_break = 224
 	let same = decodeURIComponent(url)
-	// let same2 = decodeURIComponent(url2) // second
-
 	var decode = window.atob(same)
 
 	// console.log(elem2)
-	console.log(elem)
-
+	// console.log(elem)
+	toAdd += "<div class = 'half-row'>\
+						<p class = 'top'>" + elem.line1 + "</p>\
+						<p class = 'mid'>" + elem.line2 + "</p>\
+						<p class = 'bot'>" + elem.line3 + "</p>\
+						<img src = 'data:image/png;base64, " + same + "'>\
+					</div>"
 	// let base64Decoded = decodeURIComponent( escape( window.atob( elem.url ) ) )
+	let row_counter = parseInt(elem_index) + 1
+	console.log(row_counter)
+	if (row_counter % 2 == 0 && row_counter !== 0) {
+		console.log("create outside")
+		// count = 0
+		toAdd = "<div class = 'row'>" + toAdd + "</div>"
+		container.innerHTML += toAdd
+		// console.log(toAdd)
+		toAdd = ""
+	}
 
 
-	container.innerHTML += "<div class = 'row'>\
-								<div class = 'half-row'>\
-									<p class = 'top'>" + elem.line1 + "</p>\
-									<p class = 'mid'>" + elem.line2 + "</p>\
-									<p class = 'bot'>" + elem.line3 + "</p>\
-									<img src = 'data:image/png;base64, " + same + "'>\
-								</div>\
-							</div>"
 }
+
+// 0: 1 half row, 
+
+
+
