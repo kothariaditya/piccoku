7ced70fcf1ff9837d79c01d95f2e741bb1a6da0a
ey1031 <eyudeveloper@gmail.com>
2018-03-03 18:32:08 -0800

    asdf

--- docs/app.js
+++ docs/app.js
@@ -11,28 +11,29 @@ const JSON_input = JSON.parse(httpGet('https://cors-anywhere.herokuapp.com/https
 let container = document.getElementById("container")
 
 for (let elem_index in JSON_input) {
+	console.log(elem_index)
 	let elem = JSON_input[elem_index]
-	let elem2 = JSON_input[elem_index + 1]
+	console.log(elem)
+	// let elem2 = JSON_input[elem_index + 1]
 	// var url = elem.url.replace(" ", "+").trim()
-	var url = elem.url
+	var url = elem.url // first
+	// var url2 = elem.url2 // second
 	console.log(url)
 	// console.log(elem.url.charAt(first_break))
 	// console.log(base64Decoded)
 	// first_break = 224
 	let same = decodeURIComponent(url)
+	// let same2 = decodeURIComponent(url2) // second
 
 	var decode = window.atob(same)
+
+	// console.log(elem2)
+	console.log(elem)
 
 	// let base64Decoded = decodeURIComponent( escape( window.atob( elem.url ) ) )
 
 
 	container.innerHTML += "<div class = 'row'>\
-								<div class = 'half-row'>\
-									<p class = 'top'>" + elem.line1 + "</p>\
-									<p class = 'mid'>" + elem.line2 + "</p>\
-									<p class = 'bot'>" + elem.line3 + "</p>\
-									<img src = " + elem.url + ">\
-								</div>\
 								<div class = 'half-row'>\
 									<p class = 'top'>" + elem.line1 + "</p>\
 									<p class = 'mid'>" + elem.line2 + "</p>\
